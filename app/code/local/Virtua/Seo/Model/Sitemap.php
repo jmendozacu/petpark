693c9ecc1ed1e428481a2ce8aaa4c6ea473eb66e
Krzysztof Niemiec <k.niemiec@wearevirtua.com>
2017-07-17 16:40:22 +0200

    PET-16 Html sitemap fixes

--- app/code/local/Virtua/Seo/Model/Sitemap.php
+++ app/code/local/Virtua/Seo/Model/Sitemap.php
@@ -2,26 +2,42 @@
 
 class Virtua_Seo_Model_Sitemap extends Mage_Core_Model_Abstract
 {
+    protected $_excludedIdentifiers = array(
+        'no-route', 'no-route-2', 'smartsup',
+    );
+
+    protected $_excludedCategories = array(
+        '1', '2',
+    );
+
     public function getCmsPageCollection()
     {
         $output = array();
         $cms = Mage::getModel('cms/page');
-        $collection = $cms->getCollection();
+        $storeId = Mage::app()->getStore()->getId();
+        $collection = $cms->getCollection()
+            ->addFieldToFilter('is_active', 1)
+            ->addFieldToFilter('identifier', array('nin' => $this->_getExcludedIdentifiers()))
+            ->addStoreFilter($storeId);
         $collection->getSelect()
             ->reset(Zend_Db_Select::COLUMNS)
             ->columns('title')
-            ->columns('page_id');
+            ->columns('page_id')
+            ->columns('identifier');
 
         foreach ($collection->getData() as $value) {
             $url = Mage::helper('cms/page')->getPageUrl($value['page_id']);
-            $output[$url] = $value['title'];
+            $output[$value['page_id']]['title'] = $value['title'];
+            $output[$value['page_id']]['url'] = $url;
+            $output[$value['page_id']]['identifier'] = $value['identifier'];
         }
         return $output;
     }
 
     public function getCategoryCollection()
     {
-        $category = Mage::getModel('catalog/category')->getCollection();
+        $category = Mage::getModel('catalog/category')->getCollection()
+            ->addAttributeToFilter('entity_id', array('nin' => $this->_getExcludedCategories()));
         return $category;
     }
 
@@ -30,7 +46,9 @@ class Virtua_Seo_Model_Sitemap extends Mage_Core_Model_Abstract
         $products = Mage::getModel('catalog/product')->getCollection()
             ->addFieldToFilter('is_salable', '1')
             ->addFieldToFilter('is_in_stock', '1')
-            ->addFieldToFilter(array(array('attribute'=>'visibility', 'neq'=>"1" )))
+            ->addFieldToFilter(array(
+                array('attribute'=>'visibility', 'neq'=>"1" )
+            ))
             ->addUrlRewrite();
         return $products;
     }
@@ -61,5 +79,15 @@ class Virtua_Seo_Model_Sitemap extends Mage_Core_Model_Abstract
             $output[$post['identifier']] = $post['title'];
         }
         return $output;
+    }
+
+    protected function _getExcludedIdentifiers()
+    {
+        return $this->_excludedIdentifiers;
+    }
+
+    protected function _getExcludedCategories()
+    {
+        return $this->_excludedCategories;
     }
 }
