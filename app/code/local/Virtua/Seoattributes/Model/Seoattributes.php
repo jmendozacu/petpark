91437c15a81f128d070cee0b5149bc2b4834fcaf
Krzysztof Niemiec <k.niemiec@wearevirtua.com>
2017-08-24 15:13:37 +0200

    PET-5 seo url rewritees, data inserting,

---
+++ app/code/local/Virtua/Seoattributes/Model/Seoattributes.php
@@ -0,0 +1,28 @@
+<?php
+
+class Virtua_Seoattributes_Model_Seoattributes extends Mage_Core_Model_Abstract
+{
+    protected function _construct()
+    {
+        $this->_init('virtua_seoattributes/seoattributes');
+    }
+
+    public function getSeoData($storeId = 0)
+    {
+        $seoData = array();
+        $params = Mage::app()->getRequest()->getParams();
+        $seoAttributesHelper = Mage::helper('virtua_seoattributes');
+        $categoryId = $seoAttributesHelper->categoryIsIncluded(Mage::app()->getRequest()->getParam('id'));
+        if ($categoryId) {
+            $includedParams = $seoAttributesHelper->parseAttributes($params);
+            if (!empty($includedParams)) {
+                $seoData = $this->getResource()->getSeoDataByIdAndParams($categoryId, $includedParams, true);
+                if (empty($seoData)) {
+                    $seoData = $this->getResource()->getSeoDataByIdAndParams($categoryId, $includedParams, false);
+                }
+            }
+        }
+        return $seoData;
+    }
+
+}
