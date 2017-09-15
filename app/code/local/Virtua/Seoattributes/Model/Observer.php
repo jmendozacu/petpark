8cdf9d2ee5e0da4ca1ebb070b0ec51be8c01b74d
Krzysztof Niemiec <k.niemiec@wearevirtua.com>
2017-09-15 09:18:21 +0200

    PET-9 PET-5 sitemap new items - newly generated categories

---
+++ app/code/local/Virtua/Seoattributes/Model/Observer.php
@@ -0,0 +1,36 @@
+<?php
+
+class Virtua_Seoattributes_Model_Observer
+{
+    public function addNewCategories(Varien_Event_Observer $observer)
+    {
+        try {
+            $rewritesCollection = Mage::getModel('filterurls/url')
+                ->getCollection()
+                ->addFieldToFilter('store_id', 1);
+            foreach ($this->_getExcludedAttributes() as $attr) {
+                $rewritesCollection->addFieldToFilter('attributes', array('nlike' => '%'.$attr.'%'));
+            }
+            if (!$rewritesCollection) {
+                return;
+            }
+            $collection = $observer->getCollection();
+            $items = $collection->getItems();
+            foreach ($rewritesCollection as $key => $rewrite) {
+                $item = new Varien_Object;
+                $item->setUrl($rewrite->getRequestPath());
+                $items[] = $item;
+            }
+            $collection->setItems($items);
+        } catch (Exception $exception) {
+            Mage::log($exception->getMessage());
+        }
+    }
+
+    protected function _getExcludedAttributes()
+    {
+        $helper = Mage::helper('virtua_seoattributes');
+        $exludedAttributes = $helper->getExcludedAttributes();
+        return $exludedAttributes;
+    }
+}
