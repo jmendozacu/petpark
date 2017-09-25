4e6de79af3c4a2bed04363a7059cf9194f68bb71
Krzysztof Niemiec <k.niemiec@wearevirtua.com>
2017-09-25 09:47:01 +0200

    PET-9 conflict fix

--- app/code/local/Virtua/Seoattributes/Model/Observer.php
+++ app/code/local/Virtua/Seoattributes/Model/Observer.php
@@ -2,6 +2,25 @@
 
 class Virtua_Seoattributes_Model_Observer
 {
+
+    /**
+     * Remove all seo attributes connected to updated category
+     * @param $observer
+     */
+    public function removeSeoattributesOnUpdateCategory($observer)
+    {
+        $event = $observer->getEvent();
+        $category = $event->getCategory();
+        if ($category->getId()) {
+            $seoAttributesModel = Mage::getModel('virtua_seoattributes/seoattributes');
+            $seoAttributesModel->getResource()->removeRowByCategoryId($category->getId());
+        }
+        return;
+    }
+
+    /**
+     * @param Varien_Event_Observer $observer
+     */
     public function addNewCategories(Varien_Event_Observer $observer)
     {
         try {
