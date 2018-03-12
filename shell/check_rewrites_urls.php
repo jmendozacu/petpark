b1ba8c2f86648e2e6774030886ef5fd1fd9d2b92
Tomasz Goździewski <t.gozdziewski@wearevirtua.com>
2018-03-12 10:31:29 +0100

    PET-26: Add htyp codes to file.

--- shell/check_rewrites_urls.php
+++ shell/check_rewrites_urls.php
@@ -1,25 +1,31 @@
 <?php
 $csv = array_map('str_getcsv', file('../rewrites.csv'));
 
+$devUrls = ['www.dev.petpark.sk/'];
 
-$devUrl = 'www.dev.petpark.sk/';
-$prodUrl = 'www.petpark.sk/';
+$prodUrls = ['www.petpark.sk/','www.petpark.cz/'];
 
-foreach ($csv as $cv) {
-    $url = explode(';', $cv[0]);
+foreach ($prodUrls as $key => $mainUrl) {
+    foreach ($csv as $cv) {
+        $url = explode(';', $cv[0]);
 
-    $handle = curl_init($devUrl.$url[0]);
+        $handle = curl_init($mainUrl.$url[0]);
 
-    curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
-    curl_setopt($handle, CURLOPT_NOBODY, 1);
+        curl_setopt($handle,  CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($handle, CURLOPT_NOBODY, 1);
 
-    $response = curl_exec($handle);
+        $response = curl_exec($handle);
 
-    $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
 
-     if($httpCode == 404) {
-         echo $httpCode." ----> " . $url[0] . PHP_EOL;
-     }
+        if($httpCode == 404) {
+            $code = $httpCode." ----> " . $url[0] . PHP_EOL;
+            echo $code;
+            $codes .= $code;
+        }
 
-    curl_close($handle);
+        curl_close($handle);
+    }
+    $lang = ($key == 0) ? 'sk' : 'cz';
+    file_put_contents('../urls-report-'.$lang.'.txt', $codes);
 }
