9da10e3072979fc004a934ae456c2ab5c35f9745
Tomasz Goździewski <t.gozdziewski@wearevirtua.com>
2018-05-14 15:07:21 +0200

    PET-35: remove roi hunter module files.

--- app/code/community/Businessfactory/Roihuntereasy/Block/ProductViewAnalytics.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-class Businessfactory_Roihuntereasy_Block_ProductViewAnalytics extends Businessfactory_Roihuntereasy_Block_Database
-{
-    public function getProduct() {
-        return Mage::registry('current_product');
-    }
-}
