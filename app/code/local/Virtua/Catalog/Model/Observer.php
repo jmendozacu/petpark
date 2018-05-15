f89c513865d3b244dfc3016d13c8512e359f85c2
Tomasz Goździewski <t.gozdziewski@wearevirtua.com>
2018-05-15 11:48:58 +0200

    PET-23: add new event conditions .

--- app/code/local/Virtua/Catalog/Model/Observer.php
+++ app/code/local/Virtua/Catalog/Model/Observer.php
@@ -4,7 +4,7 @@ class Virtua_Catalog_Model_Observer
 {
     public function clearMenuCache(Varien_Event_Observer $observer)
     {
-        if ($observer->getEvent()->getType() == 'catalog_category_prepare_save') {
+        if ($observer->getEvent()->getType() == 'catalog_category_prepare_save' || $observer->getEvent()->getType() == 'catalog_category_save_commit_after') {
 
             foreach (Mage::app()->getStores(false, true) as $storeCode => $store) {
                 $file = 'cat-' . $storeCode . '.cache';
