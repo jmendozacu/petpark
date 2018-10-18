0f63ec86f1f85b93ad1f26487b0417d73e19388e
Maciej Skalny <m.skalny@wearevirtua.com>
2018-10-18 15:02:04 +0200

    vat id validation

--- app/code/local/Virtua/DisableVatTax/Helper/Data.php
+++ app/code/local/Virtua/DisableVatTax/Helper/Data.php
@@ -1,7 +1,45 @@
 <?php
 
+/**
+ * Class Virtua_DisableVatTax_Helper_Data
+ */
 class Virtua_DisableVatTax_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * Regex patterns of vat id for EU countries.
+     * @var array
+     */
+    protected static $patterns = [
+        'AT' => 'U[A-Z\d]{8}',                           # Austria
+        'BE' => '(0\d{9}|\d{10})',                       # Belgium
+        'BG' => '\d{9,10}',                              # Bulgaria
+        'CY' => '\d{8}[A-Z]',                            # Cyprus
+        'CZ' => '\d{8,10}',                              # Czech Republic
+        'DE' => '\d{9}',                                 # Germany
+        'DK' => '(\d{2} ?){3}\d{2}',                     # Denmark
+        'EE' => '\d{9}',                                 # Estonia
+        'EL' => '\d{9}',                                 # Greece
+        'ES' => '[A-Z]\d{7}[A-Z]|\d{8}[A-Z]|[A-Z]\d{8}', # Spain
+        'FI' => '\d{8}',                                 # Finland
+        'FR' => '([A-Z]{2}|\d{2})\d{9}',                 # France
+        'GB' => '\d{9}|\d{12}|(GD|HA)\d{3}',             # United Kingdom
+        'HR' => '\d{11}',                                # Croatia
+        'HU' => '\d{8}',                                 # Hungary
+        'IE' => '[A-Z\d]{8}|[A-Z\d]{9}',                 # Ireland
+        'IT' => '\d{11}',                                # Italy
+        'LT' => '(\d{9}|\d{12})',                        # Lithuania
+        'LU' => '\d{8}',                                 # Luxembourg
+        'LV' => '\d{11}',                                # Latvia
+        'MT' => '\d{8}',                                 # Malta
+        'NL' => '\d{9}B\d{2}',                           # Neherlands
+        'PL' => '\d{10}',                                # Poland
+        'PT' => '\d{9}',                                 # Portugal
+        'RO' => '\d{2,10}',                              # Romania
+        'SE' => '\d{12}',                                # Sweden
+        'SI' => '\d{8}',                                 # Slovenia
+        'SK' => '\d{10}'                                 # Slovakia
+    ];
+
     /**
      * Check if customer must pay tax
      *
@@ -33,6 +71,7 @@ class Virtua_DisableVatTax_Helper_Data extends Mage_Core_Helper_Abstract
         $defaultCountry = Mage::getStoreConfig('general/country/default');
         $isCustomerOutsideDefaultCountry = $countryCode !== $defaultCountry;
         $vatNumber = $customer->getData('taxvat');
+        $vatNumber = $this->isVatNumberValid($vatNumber, $countryCode);
         $euCountries = $this->getEUCountries();
         if (!$euCountries) {
             $customerSession->setData('shouldDisableVatTax', false);
@@ -57,5 +96,32 @@ class Virtua_DisableVatTax_Helper_Data extends Mage_Core_Helper_Abstract
     public function getEUCountries()
     {
         return explode(",", Mage::getStoreConfig('general/country/eu_countries'));
+    }
+
+    /**
+     * Checks is customer has a valid vat id. Using European Commission VAT validation.
+     * @param string $vatNumber
+     * @param string $countryCode
+     * @return bool
+     */
+    public function isVatNumberValid($vatNumber, $countryCode)
+    {
+        $patterns = self::$patterns;
+        Mage::log('START', false, 'testvatid62.log', true);
+        if (strpos($vatNumber, $countryCode) === 0) {
+            Mage::log('1 IF OK', false, 'testvatid62.log', true);
+            $vatNumber = substr($vatNumber, strlen($countryCode));
+            if (preg_match('/^'.$patterns[$countryCode].'$/', $vatNumber) > 0) {
+                Mage::log('2 IF OK', false, 'testvatid62.log', true);
+                $viesClient = new SoapClient('http://ec.europa.eu/taxation_customs/vies/checkVatService.wsdl');
+                $isValid = $viesClient->checkVat(['countryCode' => $countryCode, 'vatNumber' => $vatNumber])->valid;
+            }
+        }
+        Mage::log('IS VALID: '.$isValid, false, 'testvatid62.log', true);
+        if ($isValid) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
