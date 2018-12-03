ab39d4b6e918fd7de698c40676d70b399694bd50
Maciej Skalny <m.skalny@wearevirtua.com>
2018-12-03 16:32:42 +0100

    PET-65, review changes

--- app/code/local/Virtua/BarionPayment/Model/Paymentmethod.php
+++ app/code/local/Virtua/BarionPayment/Model/Paymentmethod.php
@@ -248,7 +248,7 @@ class Virtua_BarionPayment_Model_Paymentmethod extends TLSoft_BarionPayment_Mode
         }
 
         foreach ($resultarray['Errors'] as $error) {
-            if (array_key_exists('ErrorCode', $resultarray)
+            if (array_key_exists('ErrorCode', $error)
                 && $error['ErrorCode'] === 'OriginalPaymentWasntSuccessful') {
                 return true;
             }
