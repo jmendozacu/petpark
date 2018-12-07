4b57be96fd880e59497b82b078efea9fa2a15da7
Maciej Skalny <m.skalny@wearevirtua.com>
2018-12-07 12:26:17 +0100

    PET-65, change status to complete after invoice and shipping created

--- app/code/local/Virtua/BarionPayment/Model/Adminhtml/Observer.php
+++ app/code/local/Virtua/BarionPayment/Model/Adminhtml/Observer.php
@@ -12,18 +12,29 @@
 class Virtua_BarionPayment_Model_Adminhtml_Observer
 {
     /**
-     * @param Varien_Event_Observer $observer
+     * @param $observer
+     */
+    public function manageBarionOrder($observer)
+    {
+        $orderId = Mage::app()->getRequest()->getParam('order_id');
+
+        $this->loadButtons($orderId);
+        $this->completeOrder($orderId);
+    }
+
+    /**
+     * @param int $orderId
      *
      * @return $this
      */
-    public function loadButtons($observer)
+    public function loadButtons($orderId)
     {
         $block = Mage::app()->getLayout()->getBlock('sales_order_edit');
 
         if (!$block) {
             return $this;
         }
-        $orderId = Mage::app()->getRequest()->getParam('order_id');
+
         $order = Mage::getModel('sales/order')->load($orderId);
         $status = $order->getStatus();
 
@@ -81,5 +92,43 @@ class Virtua_BarionPayment_Model_Adminhtml_Observer
         }
 
         return $qty;
+    }
+
+    /**
+     * @param array $items
+     *
+     * @return int
+     */
+    public function getQtyShipped($items)
+    {
+        $qty = 0;
+
+        foreach ($items as $item) {
+            $qty += $item->getQtyShipped();
+        }
+
+        return $qty;
+    }
+
+    /**
+     * @param int $orderId
+     */
+    public function completeOrder($orderId)
+    {
+        $order = Mage::getModel('sales/order')->load($orderId);
+        $isBarion = Mage::getModel('tlbarion/paymentmethod')
+            ->getTransModel()
+            ->loadByOrderId($orderId)
+            ->getData('real_orderid');
+
+        if ($isBarion
+            && $order->getState() == Mage_Sales_Model_Order::STATE_PROCESSING
+            && $this->getQtyInvoiced($order->getAllVisibleItems())
+            && $this->getQtyShipped($order->getAllVisibleItems())) {
+            $order->addStatusToHistory(Mage_Sales_Model_Order::STATE_COMPLETE);
+            $order->setData('state', Mage_Sales_Model_Order::STATE_COMPLETE);
+            $order->save();
+            Mage::app()->getFrontController()->getResponse()->setRedirect(Mage::helper('core/url')->getCurrentUrl());
+        }
     }
 }
